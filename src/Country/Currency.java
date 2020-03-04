d1e948d256d265463a629bf4266893be486def40
gxstav <gustavoscbarcelos@gmail.com>
2020-03-03 21:13:37 -0300

    Class Currency.

---
+++ src/Country/Currency.java
@@ -0,0 +1,11 @@
+package src.Country;
+
+public class Currency {
+    String name;
+    public void setName(String val) {
+        this.name = val;
+    }
+    public String name() {
+        return this.name;
+    }
+}
